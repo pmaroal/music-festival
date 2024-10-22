d2409d03cbb64266cdde9f89b0d0a8ccdf6b485c
PedroMaroal <pedro.maroal@gmail.com>
2024-10-22 18:53:40 +0100

    add font and create the variable structure for css

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
 
 export function css(done) {
-  src("src/scss/app.scss").pipe(sass()).pipe(dest("build/css"));
+  src("src/scss/app.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("build/css"));
 
   done();
 }
 
 export function dev() {
-  watch("src/scss/app.scss", css);
+  watch("src/scss/**/*.scss", css);
 }
