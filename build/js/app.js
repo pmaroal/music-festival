d231d3ce34a59ac8b96befbb77b6b416b92758f3
PedroMaroal <pedro.maroal@gmail.com>
2024-11-06 22:55:47 +0000

    add js file and first step of gallery section

---
+++ build/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
+});
+
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+  for (let i = 1; i <= 16; i++) {}
+}
