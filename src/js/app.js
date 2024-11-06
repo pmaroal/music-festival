21987907888ca702a126cc86983d28bef34fab2b
PedroMaroal <pedro.maroal@gmail.com>
2024-11-06 23:25:17 +0000

    import images with js

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function crearGaleria() {
+  const CANTIDAD_IMAGENES = 16;
   const galeria = document.querySelector(".galeria-imagenes");
-  for (let i = 1; i <= 16; i++) {}
+  for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = `Imagen Galeria ${i}`;
+    galeria.appendChild(imagen);
+  }
 }
