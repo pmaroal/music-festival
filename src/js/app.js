cf125835f598bf0a93c1826cf8400e9968618dc8
PedroMaroal <pedro.maroal@gmail.com>
2024-11-07 23:50:11 +0000

    finish gallery section

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,46 @@ function crearGaleria() {
     const imagen = document.createElement("IMG");
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = `Imagen Galeria ${i}`;
+    //Event handler, accion de responder a un evento del usuario
+    imagen.onclick = () => {
+      mostrarImagen(i);
+    };
+
     galeria.appendChild(imagen);
   }
+}
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = `Imagen Galeria ${i}`;
+
+  //genero el div
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+
+  //botón cerrar modal
+  const cerrarModalBtn = document.createElement("BUTTON");
+  cerrarModalBtn.textContent = "X";
+  cerrarModalBtn.classList.add("btn-cerrar");
+  cerrarModalBtn.onclick = cerrarModal;
+
+  modal.appendChild(imagen);
+  modal.appendChild(cerrarModalBtn);
+
+  //agregar al html
+  //selecciono el body
+  const body = document.querySelector("body");
+  body.classList.add("overflow-hidden");
+  //agrego el modal en el body
+  body.appendChild(modal);
+}
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fadeOut");
+  setTimeout(() => {
+    modal?.remove();
+    const body = document.querySelector("body");
+    body.classList.remove("overflow-hidden");
+  }, 500);
 }
