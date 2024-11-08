d3cdfb5f663cf3470043e48b201ceaf542873f18
PedroMaroal <pedro.maroal@gmail.com>
2024-11-08 18:24:05 +0000

    first step to fixed header

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+  navegacionFija();
   crearGaleria();
 });
+function navegacionFija() {
+  const header = document.querySelector("header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+
+  window.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 1) {
+      header.classList.add;
+    } else {
+      header.classList.remove("fixed");
+    }
+  });
+}
 
 function crearGaleria() {
   const CANTIDAD_IMAGENES = 16;
