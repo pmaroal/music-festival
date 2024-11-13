deb79dd37437450df019b50b07515f5ca3c1f9c7
PedroMaroal <pedro.maroal@gmail.com>
2024-11-13 23:46:55 +0000

    add sharp for galery

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
+import path from "path";
+import fs from "fs";
+
 import { src, dest, watch, series } from "gulp";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 
 const sass = gulpSass(dartSass);
+
+import sharp from "sharp";
 
 export function js(done) {
   src("src/js/app.js").pipe(dest("build/js"));
@@ -18,9 +23,37 @@ export function css(done) {
   done();
 }
 
+export async function crop(done) {
+  const inputFolder = "src/img/gallery/full";
+  const outputFolder = "src/img/gallery/thumb";
+  const width = 250;
+  const height = 180;
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder, { recursive: true });
+  }
+  const images = fs.readdirSync(inputFolder).filter((file) => {
+    return /\.(jpg)$/i.test(path.extname(file));
+  });
+  try {
+    images.forEach((file) => {
+      const inputFile = path.join(inputFolder, file);
+      const outputFile = path.join(outputFolder, file);
+      sharp(inputFile)
+        .resize(width, height, {
+          position: "centre",
+        })
+        .toFile(outputFile);
+    });
+
+    done();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export function dev() {
   watch("src/scss/**/*.scss", css);
   watch("src/js/**/*.js", js);
 }
 
-export default series(js, css, dev);
+export default series(crop, js, css, dev);
